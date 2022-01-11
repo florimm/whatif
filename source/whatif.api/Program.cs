37afe498f0445feffbff08318bdc5f9676a575d6
florimmaxhuni <florim.maxhuni@gmail.com>
2022-01-11 23:26:01 +0100

    Changed to test build

--- source/whatif.api/Program.cs
+++ source/whatif.api/Program.cs
@@ -9,7 +9,8 @@ using WhatIf.Api.Actors;
 using WhatIf.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<ITokenService>(new TokenService(builder.Configuration["Jwt:Key"], builder.Configuration["Jwt:Issuer"]));
+builder.Services.AddSingleton<ITokenService>(
+    new TokenService(builder.Configuration["Jwt:Key"], builder.Configuration["Jwt:Issuer"]));
 
 builder.WebHost.ConfigureKestrel(options => options.ListenLocalhost(5178));
 
